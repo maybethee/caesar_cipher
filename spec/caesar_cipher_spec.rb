1319fdca38a34a054d7ae4d2296ae84735a74c1e
maybethee <jozalamun@gmail.com>
2023-05-07 19:15:40 -0400

    Write test for punctuation use

--- spec/caesar_cipher_spec.rb
+++ spec/caesar_cipher_spec.rb
@@ -4,11 +4,15 @@ require './lib/caesar_cipher'
 describe "#caesar_cipher" do
 
   it "shifts characters by the correct amount" do
-    expect(caesar_cipher("Hello world", 16)).to match(/Xubbe mehbt/)
+    expect(caesar_cipher('Hello world', 16)).to match(/Xubbe mehbt/)
   end
 
   it "wraps all z's to a's" do
     expect(caesar_cipher('Zzzzz', 1)).to match(/Aaaaa/)
+  end
+
+  it "ignores punctuation" do
+    expect(caesar_cipher('Hello world...?', 10)).to match(/Rovvy gybvn...?/)
   end
 end
 
